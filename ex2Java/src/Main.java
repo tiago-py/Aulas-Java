d6691df417a5c43b1fce8e8b877773611e3046d8
tiago-py <tiago.braga0012@gmail.com>
2023-08-29 15:19:33 -0300

    metodos construtores

--- ex2Java/src/Main.java
+++ ex2Java/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa("Tiago", "123.456.789-01");
+        Pessoa pessoa1 = new Pessoa("Tiago", "123.456.789-01", 21);
         pessoa1.exibirPessoa();
     }
 }
--- ex2Java/src/Pessoa.java
+++ ex2Java/src/Pessoa.java
@@ -2,14 +2,17 @@ public class Pessoa {
 
         public String nome;
         public String CPF;
-
-        public Pessoa(String n, String a){
+        public int idade;
+        public Pessoa(String n, String a, int x){
             nome = n;
             CPF = a;
+            idade = x;
         }
         public void exibirPessoa(){
             System.out.println(this.nome);
             System.out.println(this.CPF);
+            System.out.println(this.idade);
+
         }
 
 
